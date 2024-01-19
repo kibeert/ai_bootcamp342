8acb1e863fd10a72a3ccdb962bd6ecd7cf1358c3
francis-100 <85951839+francis-100@users.noreply.github.com>
2024-01-19 15:33:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,83 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-diabetes_model = pickle.load(open('diseases_model.sav', 'rb'))
+diseases_model = pickle.load(open('diseases_model.sav', 'rb'))
 
 # Diseases Prediction Page title
-st.title('Diseases Prediction using ML')
+st.title("France's Smart Health Checker")
+
+# Define a subset of common diseases
+common_diseases = [
+    "Paroymsal Positional Vertigo", "AIDS", "Acne", "Alcoholic Hepatitis", "Allergy", "Arthritis",
+    "Bronchial Asthma", "Cervical Spondylosis", "Chickenpox", "Chronic Cholestasis", "Common Cold",
+    "Dengue", "Diabetes", "Dimorphic Hemorrhoids (Piles)", "Drug Reaction", "Fungal Infection", "GERD",
+    "Gastroenteritis", "Heart Attack", "Hepatitis B", "Hepatitis C", "Hepatitis D", "Hepatitis E",
+    "Hypertension", "Hyperthyroidism", "Hypoglycemia", "Hypothyroidism", "Impetigo", "Jaundice", "Malaria",
+    "Migraine", "Osteoarthritis", "Paralysis (Brain Hemorrhage)", "Peptic Ulcer Disease", "Pneumonia",
+    "Psoriasis", "Tuberculosis", "Typhoid", "Urinary Tract Infection", "Varicose Veins", "Hepatitis A",
+]
+
+# Define a subset of common symptoms
+common_symptoms = [
+    "Itching", "Skin Rash", "Continuous Sneezing", "Chills", "Joint Pain", "Vomiting",
+    "Fatigue", "Weight Loss", "Cough", "High Fever", "Headache", "Yellowish Skin"
+]
 
 # getting the input data from the user
 col1, col2, col3 = st.columns(3)
 
+def validate_input(value):
+    if value not in ['0', '1']:
+        return '0'  # Default to 0 if input is not 0 or 1
+    return value
+
 with col1:
-    Fever = st.text_input('Fever check', value='0')
+    Itching = st.text_input('Itching', value='0', max_chars=1)
+    Itching = validate_input(Itching)
 
 with col2:
-    Chills = st.text_input('Chills check', value='0')
+    Skin_Rash = st.text_input('Skin Rash', value='0', max_chars=1)
+    Skin_Rash = validate_input(Skin_Rash)
 
 with col3:
-    Congestion = st.text_input('Congestion check', value='0')
+    Continuous_Sneezing = st.text_input('Continuous Sneezing', value='0', max_chars=1)
+    Continuous_Sneezing = validate_input(Continuous_Sneezing)
 
 with col1:
-    Cough = st.text_input('Cough check', value='0')
+    Chills = st.text_input('Chills', value='0', max_chars=1)
+    Chills = validate_input(Chills)
 
 with col2:
-    Fatigue = st.text_input('Fatigue check', value='0')
+    Joint_Pain = st.text_input('Joint Pain', value='0', max_chars=1)
+    Joint_Pain = validate_input(Joint_Pain)
 
 with col3:
-    Muscle_Aches = st.text_input('Muscle Ache check', value='0')
+    Vomiting = st.text_input('Vomiting', value='0', max_chars=1)
+    Vomiting = validate_input(Vomiting)
 
 with col1:
-    Headache = st.text_input('Headache check', value='0')
+    Fatigue = st.text_input('Fatigue', value='0', max_chars=1)
+    Fatigue = validate_input(Fatigue)
 
 with col2:
-    Diarrhea = st.text_input('Diarrhea check', value='0')
+    Weight_Loss = st.text_input('Weight Loss', value='0', max_chars=1)
+    Weight_Loss = validate_input(Weight_Loss)
 
 with col3:
-    Nausea = st.text_input('Nausea check', value='0')
+    Cough = st.text_input('Cough', value='0', max_chars=1)
+    Cough = validate_input(Cough)
 
 with col1:
-    Vomiting = st.text_input('Vomiting check', value='0')
+    High_Fever = st.text_input('High Fever', value='0', max_chars=1)
+    High_Fever = validate_input(High_Fever)
+
+with col2:
+    Headache = st.text_input('Headache', value='0', max_chars=1)
+    Headache = validate_input(Headache)
+
+with col3:
+    Yellowish_Skin = st.text_input('Yellowish Skin', value='0', max_chars=1)
+    Yellowish_Skin = validate_input(Yellowish_Skin)
 
 # code for Prediction
 disease_diagnosis = ''
@@ -46,53 +86,22 @@ disease_diagnosis = ''
 # creating a button for Prediction
 if st.button('Disease Test Result'):
 
-    user_input = [Fever, Chills, Congestion, Cough, Fatigue, Muscle_Aches, Headache, Diarrhea, Nausea, Vomiting]
+    user_input = [Itching, Skin_Rash, Continuous_Sneezing, Chills, Joint_Pain, Vomiting,
+                  Fatigue, Weight_Loss, Cough, High_Fever, Headache, Yellowish_Skin]
 
-    user_input = [float(x) for x in user_input]
-
-    disease_prediction = diabetes_model.predict([user_input])
-
-    if disease_prediction[0] == 1:
-        disease_diagnosis = 'The patient is suffering from Common Cold.'
-    elif disease_prediction[0] == 2:
-        disease_diagnosis = 'The patient is suffering from Flu.'
-    elif disease_prediction[0] == 3:
-        disease_diagnosis = 'The patient is suffering from COVID-19.'
-    elif disease_prediction[0] == 4:
-        disease_diagnosis = 'The patient is suffering from Gastroenteritis.'
-    elif disease_prediction[0] == 5:
-        disease_diagnosis = 'The patient is suffering from Hepatitis.'
-    elif disease_prediction[0] == 6:
-        disease_diagnosis = 'The patient is suffering from Respiratory Syncytial Virus (RSV).'
-    elif disease_prediction[0] == 7:
-        disease_diagnosis = 'The patient is suffering from Strep Throat.'
-    elif disease_prediction[0] == 8:
-        disease_diagnosis = 'The patient is suffering from Salmonella.'
-    elif disease_prediction[0] == 9:
-        disease_diagnosis = 'The patient is suffering from Tuberculosis.'
-    elif disease_prediction[0] == 10:
-        disease_diagnosis = 'The patient is suffering from Whooping Cough.'
-    elif disease_prediction[0] == 11:
-        disease_diagnosis = 'The patient is suffering from Chlamydia.'
-    elif disease_prediction[0] == 12:
-        disease_diagnosis = 'The patient is suffering from Gonorrhea.'
-    elif disease_prediction[0] == 13:
-        disease_diagnosis = 'The patient is suffering from Urinary Tract Infections (UTIs).'
-    elif disease_prediction[0] == 14:
-        disease_diagnosis = 'The patient is suffering from E. coli.'
-    elif disease_prediction[0] == 15:
-        disease_diagnosis = 'The patient is suffering from Clostridioides difficile.'
-    elif disease_prediction[0] == 16:
-        disease_diagnosis = 'The patient is suffering from Ringworm.'
-    elif disease_prediction[0] == 17:
-        disease_diagnosis = 'The patient is suffering from Fungal Nail Infections.'
-    elif disease_prediction[0] == 18:
-        disease_diagnosis = 'The patient is suffering from Thrush.'
-    elif disease_prediction[0] == 19:
-        disease_diagnosis = 'The patient is suffering from Giardiasis.'
-    elif disease_prediction[0] == 20:
-        disease_diagnosis = 'The patient is suffering from Toxoplasmosis.'
+     # Check if all inputs are zero
+    if all(x == '0' for x in user_input):
+        disease_diagnosis = "Everything looks great healthwise. Keep taking care of yourself!"
     else:
-        disease_diagnosis = 'The patient is suffering from some disease.'
+        user_input = [float(x) for x in user_input]
+        disease_prediction = diseases_model.predict([user_input])
+
+        if 1 <= disease_prediction[0] <= len(common_diseases):
+            disease_diagnosis = f'You might be suffering from {common_diseases[disease_prediction[0] - 1]}.'
+        else:
+            disease_diagnosis = 'You might be suffering from some disease.'
+
+        # Add a message about consulting a professional doctor
+        disease_diagnosis += ' Kindly see a professional doctor for proper check-up and medication.'
 
 st.success(disease_diagnosis)
