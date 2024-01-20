48c539981d109ed2572b0d08f5d7da2aee0d3031
francis-100 <85951839+francis-100@users.noreply.github.com>
2024-01-20 09:47:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,72 +24,26 @@ common_symptoms = [
     "Fatigue", "Weight Loss", "Cough", "High Fever", "Headache", "Yellowish Skin"
 ]
 
+def validate_input(symptom_input):
+    return '0' if symptom_input not in ['0', '1'] else symptom_input
+
 # getting the input data from the user
 col1, col2, col3 = st.columns(3)
 
-def validate_input(value):
-    if value not in ['0', '1']:
-        return '0'  # Default to 0 if input is not 0 or 1
-    return value
+user_input = []
 
-with col1:
-    Itching = st.text_input('Itching', value='0', max_chars=1)
-    Itching = validate_input(Itching)
-
-with col2:
-    Skin_Rash = st.text_input('Skin Rash', value='0', max_chars=1)
-    Skin_Rash = validate_input(Skin_Rash)
-
-with col3:
-    Continuous_Sneezing = st.text_input('Continuous Sneezing', value='0', max_chars=1)
-    Continuous_Sneezing = validate_input(Continuous_Sneezing)
-
-with col1:
-    Chills = st.text_input('Chills', value='0', max_chars=1)
-    Chills = validate_input(Chills)
-
-with col2:
-    Joint_Pain = st.text_input('Joint Pain', value='0', max_chars=1)
-    Joint_Pain = validate_input(Joint_Pain)
-
-with col3:
-    Vomiting = st.text_input('Vomiting', value='0', max_chars=1)
-    Vomiting = validate_input(Vomiting)
-
-with col1:
-    Fatigue = st.text_input('Fatigue', value='0', max_chars=1)
-    Fatigue = validate_input(Fatigue)
-
-with col2:
-    Weight_Loss = st.text_input('Weight Loss', value='0', max_chars=1)
-    Weight_Loss = validate_input(Weight_Loss)
-
-with col3:
-    Cough = st.text_input('Cough', value='0', max_chars=1)
-    Cough = validate_input(Cough)
-
-with col1:
-    High_Fever = st.text_input('High Fever', value='0', max_chars=1)
-    High_Fever = validate_input(High_Fever)
-
-with col2:
-    Headache = st.text_input('Headache', value='0', max_chars=1)
-    Headache = validate_input(Headache)
-
-with col3:
-    Yellowish_Skin = st.text_input('Yellowish Skin', value='0', max_chars=1)
-    Yellowish_Skin = validate_input(Yellowish_Skin)
+for i, symptom in enumerate(common_symptoms):
+    with col1 if i % 3 == 0 else col2 if i % 3 == 1 else col3 as col:
+        symptom_input = st.text_input(symptom, value='0', max_chars=1)
+        validated_input = validate_input(symptom_input)
+        user_input.append(validated_input)
 
 # code for Prediction
 disease_diagnosis = ''
 
 # creating a button for Prediction
 if st.button('Disease Test Result'):
-
-    user_input = [Itching, Skin_Rash, Continuous_Sneezing, Chills, Joint_Pain, Vomiting,
-                  Fatigue, Weight_Loss, Cough, High_Fever, Headache, Yellowish_Skin]
-
-     # Check if all inputs are zero
+    # Check if all inputs are zero
     if all(x == '0' for x in user_input):
         disease_diagnosis = "Everything looks great healthwise. Keep taking care of yourself!"
     else:
